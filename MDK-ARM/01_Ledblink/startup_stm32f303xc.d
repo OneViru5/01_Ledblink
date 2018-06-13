1062493592e4be66bee4ac9f87ff62ff048da05a
= <=>
2018-06-13 09:08:15 +0700

    commit to repository

---
+++ MDK-ARM/01_Ledblink/startup_stm32f303xc.d
@@ -0,0 +1,1 @@
+01_ledblink\startup_stm32f303xc.o: startup_stm32f303xc.s
---
+++ MDK-ARM/RTE/_01_Ledblink/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '01_Ledblink' 
+ * Target:  '01_Ledblink' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
